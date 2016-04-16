a597963c3d18e1feceadb86d7f4306bcc3129fab
Reid <reidgill20@gmail.com>
2016-04-16 06:19:29 -0400

    Full and complete and ready to be built onto google cardboard. Excess files ridded.

---
+++ CIS4930_Waves/Assets/Scripts/AudioSlave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using AudioItems;
+
+public class AudioSlave : MonoBehaviour {
+
+	public bool LookingAt;
+	public int InstrumentID;
+	public GameObject AudioSystem;
+	public GameObject ParticleSystem;
+	private AudioSource Audio;
+	private bool startParticles = false;
+
+
+	// Use this for initialization
+	void Start() {
+		LookingAt = false;
+		Audio = (AudioSource)AudioSystem.GetComponents<AudioSource> ().GetValue(InstrumentID);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (LookingAt && Input.GetButtonDown ("Fire1")) {
+			Audio.mute = !Audio.mute;
+			ToggleParticles();
+		}
+	}
+
+	public void ToggleLookingAt() {
+		LookingAt = !LookingAt;
+	}
+
+	public void ToggleParticles() {
+		startParticles = !startParticles;
+		ParticleSystem.SetActive (startParticles);
+	}
+
+}
---
+++ CIS4930_Waves/Assets/Scripts/EInstrument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AudioItems
+{
+	public enum EInstrument
+	{
+		//Tropical
+		SteelDrum, //0
+		SteelGuitar, //1
+		PanFlute, //2
+		Bongos, //3
+
+		//Classical
+		Harp, //4
+		Cello, //5
+		Violin, //6
+		ClassicalDrum, //7 
+
+		//Rock
+		ElectricGuitar, //8
+		BassGuitar, //9
+		GuitarWithAmp, //10
+		DrumSet, //11
+
+		//Electronic
+		ElectricKeyboard, //12
+		BassSynth, //13
+		PadSynth, //14
+		ClubPercussion, //15
+
+		//Vocals
+		Vocal //16
+	}
+}
+
